392e941b494350545a4917f7a9f5020aae6bc546
Miroslav Poledňák <miroslavpolednak@MiroslavMac.local>
2019-06-01 15:25:07 +0200

    Add some code

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.error("Hello word change");
+var person = {
+    age: "11",
+    personName: "Miras",
+    address: {
+        street: "Tachlovice",
+        postalCode: "25217"
+
+    }
+};
+
+
+class personObj {
+    /**
+     * @param {number} age
+     * @param {string} personName
+     */
+    constructor(age, personName) {
+        var lastname = "Polednak";
+        this.age = age;
+        this.personName = personName;
+        this.getLastname = () => {
+            return lastname;
+        };
+    }
+    Say() {
+        console.log(`${this.age} ${this.personName} ${this.getLastname()} `);
+    };
+}
+
+var prObj = new personObj(32, "Miras");
+prObj.Say();
